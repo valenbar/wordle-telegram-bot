02533c85da6e8ec0c74ecf99634fc4f64eee803a
Valentin Bartschies <valentin.bartschies@tuhh.de>
2022-01-18 01:08:57 +0100

    improved design + scalability

--- ImageExport.py
+++ ImageExport.py
@@ -1,6 +1,4 @@
 from PIL import Image, ImageDraw, ImageFont, ImageColor
-import sys
-
 
 class WordleColors:
     black = (18, 18, 18)
@@ -20,16 +18,12 @@ class ImageExport():
         self.board_y = y
 
     def get_image(self, color_matrix, guessed_words):
-        # font stuff
         fnt=ImageFont.truetype('./res/FiraCode-SemiBold.ttf', size=40)
         letter_size = {}
         for c in "abcdefghijklmnopqrstuvwxyzäöüABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜ":
             letter_size[c] = fnt.getsize(c)
 
         c = WordleColors()
-        # board_x = len(color_matrix[0])
-        # board_y = len(color_matrix)
-
         TILE = 80
         LINE_THICKNESS = 4
         board_width = TILE * self.board_x + LINE_THICKNESS
@@ -40,14 +34,28 @@ class ImageExport():
         im = Image.new('RGB' , (board_width +  2, board_height + 2), color=c.black)
         draw = ImageDraw.Draw(im)
 
-
-
-        # fnt=ImageFont.truetype('arial.ttf', size=40)
-        # Todo scalability
-        for i in range(0, 5):
-            for j in range(0, 6):
-                if len(guessed_words) > j: l_pos = (i*TILE+TILE/2 - letter_size[guessed_words[j][i]][0]/2+LINE_THICKNESS/2, j*TILE+TILE/2 - letter_size[guessed_words[j][i]][1]/2-LETTER_OFFSET+LINE_THICKNESS/2)
-                draw.rectangle([(i*TILE+LINE_THICKNESS/2, j*TILE+LINE_THICKNESS/2), ((i+1)*TILE+LINE_THICKNESS/2, (j+1)*TILE+LINE_THICKNESS/2)], width=LINE_THICKNESS, fill=colors[j][i], outline=c.black)
-                if len(guessed_words) > j: draw.text(l_pos, guessed_words[j][i], fill=c.white, font=fnt)
-        # im.show()
+        for i in range(0, self.board_x):
+            for j in range(0, self.board_y):
+                if j < len(guessed_words):
+                    draw.rectangle(
+                        [(i * TILE + LINE_THICKNESS / 2, j * TILE + LINE_THICKNESS / 2), ((i + 1) * TILE + LINE_THICKNESS / 2, (j + 1) * TILE + LINE_THICKNESS / 2)],
+                        width=LINE_THICKNESS, fill=colors[j][i], outline=c.black)
+                if len(guessed_words) > j:
+                    l_pos = (   i * TILE + TILE / 2 - letter_size[guessed_words[j][i]][0] / 2 + LINE_THICKNESS / 2,
+                                j * TILE + TILE / 2 - letter_size[guessed_words[j][i]][1] / 2 - LETTER_OFFSET+LINE_THICKNESS / 2)
+                else:
+                    shrink_rec = 4
+                    draw.rectangle(
+                        [(
+                            i * TILE + LINE_THICKNESS / 2 + shrink_rec,
+                            j * TILE + LINE_THICKNESS / 2 + shrink_rec
+                        ),
+                        (
+                            (i + 1) * TILE + LINE_THICKNESS / 2 - shrink_rec,
+                            (j + 1) * TILE + LINE_THICKNESS / 2 - shrink_rec
+                        )],
+                        width=2, fill=c.black, outline=c.lgray
+                    )
+                if len(guessed_words) > j:
+                    draw.text(l_pos, guessed_words[j][i], fill=c.white, font=fnt)
         return im
